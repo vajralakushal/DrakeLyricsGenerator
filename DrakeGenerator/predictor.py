b9e91656555a759fa19a1b04f0e4b1a4276bd205
Kushal Vajrala <vajrala.kushal@gmail.com>
2021-01-10 15:43:10 -0600

    stored objects in pickle files to not have to worry about them

--- DrakeGenerator/predictor.py
+++ DrakeGenerator/predictor.py
@@ -7,21 +7,40 @@ from flask_cors import CORS
 import numpy as np
 import random
 import io
+import pickle
 
 app = Flask(__name__)
 CORS(app)
 
-model_1 = keras.models.load_model('/Users/ayaanmahimwala/Coding/DrakeLyricsGenerator/DrakeGenerator/model1')
+model_1 = keras.models.load_model('/Users/kushalvajrala/ProgrammingProjects/DrakeLyricsGenerator/DrakeGenerator/model1')
 
-path = '/Users/ayaanmahimwala/Coding/DrakeLyricsGenerator/DrakeGenerator/drake_dataset/drake_lyrics.txt'
+path = '/Users/kushalvajrala/ProgrammingProjects/DrakeLyricsGenerator/DrakeGenerator/drake_dataset/drake_lyrics.txt'
 with io.open(path, encoding="utf-8") as f:
     text = f.read().lower()
 text = text.replace("\n", " ")  # We remove newlines chars for nicer display
 
 chars = sorted(list(set(text)))
-char_indices = dict((c, i) for i, c in enumerate(chars))
-indices_char = dict((i, c) for i, c in enumerate(chars))
+#char_indices = dict((c, i) for i, c in enumerate(chars))
+#indices_char = dict((i, c) for i, c in enumerate(chars))
 diversity = 0.5
+
+
+
+file_name1 = "chari"
+file_name2 = "ichar"
+outfile1 = open(file_name1, "rb")
+outfile2 = open(file_name2, "rb")
+
+#pickle.dump(char_indices, outfile1)
+#pickle.dump(indices_char, outfile2)
+
+
+char_indices = pickle.load(outfile1)
+indices_char = pickle.load(outfile2)
+outfile1.close()
+outfile2.close()
+print(char_indices)
+print(indices_char)
 
 def sample(preds, temperature=1.0):
     # helper function to sample an index from a probability array
